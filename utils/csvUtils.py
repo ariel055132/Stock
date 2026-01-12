a358e96b5ffa37ba13990cfb67ae96194072b367
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2026-01-12 13:05:05 +0800

    Add load_from_csv function to csvUtils module for loading DataFrames from CSV files

--- utils/csvUtils.py
+++ utils/csvUtils.py
@@ -8,3 +8,13 @@ def save_to_csv(data: pd.DataFrame, fileName: str):
     """
     data.to_csv(fileName, index=False)
     print(f"Data saved to {fileName}")
+
+def load_from_csv(fileName: str) -> pd.DataFrame:
+    """Load DataFrame from a CSV file.
+
+    :param fileName: Name of the input CSV file
+    :return: Loaded DataFrame
+    """
+    data = pd.read_csv(fileName, encoding='utf-8')
+    print(f"Data loaded from {fileName}")
+    return data
